fdf5b4ab8de70a390417ad480da94222d213a395
Bryan Fullerton <bjf@bryanfullerton.com>
2014-05-26 15:10:43 -0400

    first working copy

--- README.md
+++ README.md
@@ -3,5 +3,13 @@ akamai-clear-rest
 
 Just a Python3 script to clear Akamai cache via their new REST API.
 
-### ToDo:
-* handle multiple ARL objects with the --object param (or multiple --object params)
+    $ ./akamai-clear-rest.py --help
+    usage: akamai-clear-rest.py [-h] (--cpcode CPCODE | --object OBJECT) --user
+                                USER --passwd PASSWD
+    
+    optional arguments:
+      -h, --help       show this help message and exit
+      --cpcode CPCODE  CP code to clear
+      --object OBJECT  Object to clear (can specify multiple times)
+      --user USER      Akamai user
+      --passwd PASSWD  Akamai password
--- akamai-clear-rest.py
+++ akamai-clear-rest.py
@@ -2,22 +2,35 @@
 
 import sys
 import urllib
-import urllib3
 import json
 import argparse
+import requests
 
+ccu_base = "https://api.ccu.akamai.com"
+ccu_endpoint = ccu_base+"/ccu/v2/queues/default"
+headers = {'content-type': 'application/json'}
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("--cpcode", help="CP code to clear")
-group.add_argument("--object", help="Object to clear")
+group.add_argument("--object", help="Object to clear (can specify multiple times)", action='append')
 parser.add_argument("--user", help="Akamai user", required=True)
-parser.add_argument("--pass", help="Akamai password", required=True)
+parser.add_argument("--passwd", help="Akamai password", required=True)
 args = parser.parse_args()
 
 if args.cpcode is not None:
     postdata = json.dumps({'type': 'cpcode', 'objects': [args.cpcode]})
 else:
-    postdata = json.dumps({'objects': [args.object]})
+    postdata = json.dumps({'objects': args.object})
 
-
+r = requests.post(ccu_endpoint, data=postdata, headers=headers, auth=(args.user, args.passwd))
+if r.encoding is not None:
+    print ("Error, unexpected data received:\n ",r.text)
+    exit(1)
+else:
+    resp = json.loads(r.text)
+    if resp['httpStatus'] < 200 or resp['httpStatus'] > 299:
+        print ("Error, request not submitted\n\thttpStatus: ", resp['httpStatus'], "\n\ttitle: ", resp['title'], "\n\tdetail: ", resp['detail'], "\n\tdescribedBy: ", resp['describedBy'])
+        exit(1)
+    else:
+        print ("Request submitted OK\n\tEstimated time to clear: ", resp['estimatedSeconds']/60, "minutes\n\tCheck URL: ", ccu_base+resp['progressUri'])
